da1717c9991d15eefa7fad48e16f745523855043
Mark Molnar <molnarpw@gmail.com>
2018-05-14 15:37:13 +0200

    New untested libs

--- capsule.lua
+++ capsule.lua
@@ -3,7 +3,7 @@ function Capsule(modules)
 	self.__ContainerContext = {
 		__Modules = {},
 	}
-	
+
 	self.getModuleCode = function(self, moduleName)
 		local fileToModule = fileOpen(moduleName .. ".capsule")
 		local moduleCode = "return function(context)\nlocal self = setmetatable(context, {})\n"
@@ -11,22 +11,22 @@ function Capsule(modules)
 		moduleCode = moduleCode .. code
 		fileClose(fileToModule)
 		moduleCode = moduleCode .. "\nreturn self\nend"
-		
+
 		return moduleCode
 	end
-	
-	self.loadIntoContext = function(self, moduleName)		
+
+	self.loadIntoContext = function(self, moduleName)
 		local moduleCode = self:getModuleCode(moduleName)
-	
+
 		self.__ContainerContext.__Modules[moduleName] = {
 			__MODULENAME = moduleName,
-			
+
 			globalContext = self,
-			
+
 			getGlobalContext = function(self)
 				return self.globalContext
 			end,
-			
+
 			import = function(self, moduleName)
 				if string.match(moduleName, "@std") then
 					local moduleNameParts = split(moduleName, "/")
@@ -34,26 +34,26 @@ function Capsule(modules)
 					return loadstring(moduleCode)(self)({})
 				else
 				local moduleCode = self:getGlobalContext():getModuleCode(moduleName)
-				return loadstring(moduleCode)(self)(self)	
+				return loadstring(moduleCode)(self)(self)
 				end
 			end,
 		}
-		
+
 		local snippet = loadstring(moduleCode)()(self.__ContainerContext.__Modules[moduleName])
 		local newSelf = setmetatable(self.__ContainerContext.__Modules[moduleName], snippet)
 		snippet:init(newSelf)
-				
+
 		return newSelf
 	end
-	
+
 	self.get = function(self, moduleName)
 		return self.__ContainerContext.__Modules[moduleName]
 	end
-	
+
 	for _, moduleData in pairs(modules) do
 		self:loadIntoContext(moduleData.name, moduleData.code)
 	end
-	
+
 	return self
 end
 
@@ -61,7 +61,7 @@ local simpleModule = [[
 return function(context)
 	local self = setmetatable(context, {})
 	self.value = 5
-	
+
 	self.init = function(self)
 		outputDebugString(inspect(self.value))
 	end
@@ -72,5 +72,5 @@ end
 
 local capsule = Capsule({
 	{name = "test"},
-}) 
+})
 --> Module 1 initialized.
